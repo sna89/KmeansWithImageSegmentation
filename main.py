615f09a474c78f275c2c79df19737d425fe2135b
sna89 <arbeln315@gmail.com>
2021-09-05 18:13:55 +0300

    remove comment from main

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from ImageSegmentationTask import ImageSegmentationTask
 import os
+from IrisDataset import IrisDataset
+from kmeans import Kmeans, plot_elbow
 
 
 if __name__ == "__main__":
-    # iris_ds = IrisDataset().get_data()
-    # scores = []
-    # for k in range(1, 11):
-    #     kmeans = Kmeans(k, 0.001)
-    #     centroid_sample_mapping, score = kmeans.fit(iris_ds, iterations=100)
-    #     scores.append(score)
-    # plot_elbow(scores)
+    iris_ds = IrisDataset().get_data()
+    scores = []
+    for k in range(1, 11):
+        kmeans = Kmeans(k, 0.001)
+        centroid_sample_mapping, score = kmeans.fit(iris_ds, iterations=100)
+        scores.append(score)
+    plot_elbow(scores)
 
     segmentation = ImageSegmentationTask()
     path = os.path.join(os.getcwd(), "Images")
